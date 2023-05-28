c34f2fae0a1afe2a357c1304fce42a15687d202b
jruizsalto <jruizsalto@gmail.com>
2023-05-28 17:03:12 +0200

    Testing of images resized with Sharp. The 'Uploads' folder, which is no longer needed, has been deleted.

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 const express = require('express');  // Importing the Express framework
 const multer = require('multer');  // Importing the Multer library for handling file uploads
+const sharp = require('sharp'); //Importing the Sharp Library for resize images
+const fs = require('fs');  // Importing the fs module for file system operations
+const path = require('path');  // Importing the path module for working with file paths
 
 const app = express();  // Creating an Express application
 const port = 5000;  // Setting the port number for the server
+// Configuring Multer to use 'multer.memoryStorage()' as the storage engine
+const upload = multer({ storage: multer.memoryStorage() });
 
-const upload = multer({ dest: 'uploads/' });  // Creating a Multer middleware for file upload
+// Post route for the 'task' request
+app.post('/task', upload.single('img'), async function (req, res) {
+    const file = req.file;
+    const originalName = req.file.originalname;
+    const folderName = path.parse(originalName).name;
+    const extension = path.extname(originalName);
+    const destination = './output/' + folderName;
 
-// Route for handling the POST request for '/task'
-app.post('/task', upload.single('img'), function (req, res) {
-    const file = req.file;  // Accessing the uploaded file
-    console.log(file);
-    res.send('Post');  // Sending a response of 'Post'
+    // Checking if the destination folder exists, and creating it if it doesn't
+    if (!fs.existsSync(destination)) {
+        fs.mkdirSync(destination, { recursive: true });
+    };
+
+    // Resizing the image using the 'Sharp' library
+    const resizedImage = sharp(file.buffer).resize(1024, null, {
+        fit: 'contain',
+        background: '#FFF',
+        withoutEnlargement: true
+    }).toFile(destination + '/' + originalName + extension, (err, info) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Imagen modificada guardada correctamente:', info);
+        }
+    });
+
+    // Converting the resized image to a Buffer
+    const resizedImageBuffer = await resizedImage.toBuffer();
+    console.log(resizedImageBuffer);
+    res.send({ resizedImage: resizedImageBuffer });
 });
 
 // Starting the server and listening on the specified port
 app.listen(port, () => {
-    console.log('APP running on port: ' + port);  // Logging a message indicating the server is running
+    console.log('APP running on port: ' + port);
 });
