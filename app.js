450b8e565eaeef12e32ea16ab193c7e58874af22
jruizsalto <jruizsalto@gmail.com>
2023-05-28 16:31:43 +0200

    Multer installed and configured, test to retrieve file from the request using Postman. Added exampleImages folder with the images used for testing.

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require('express');  // Importing the Express framework
+const multer = require('multer');  // Importing the Multer library for handling file uploads
 
-const app = express();
-const port = 5000;
+const app = express();  // Creating an Express application
+const port = 5000;  // Setting the port number for the server
 
-app.get('/task', function (req, res) {
-    res.send('GET');
+const upload = multer({ dest: 'uploads/' });  // Creating a Multer middleware for file upload
+
+// Route for handling the POST request for '/task'
+app.post('/task', upload.single('img'), function (req, res) {
+    const file = req.file;  // Accessing the uploaded file
+    console.log(file);
+    res.send('Post');  // Sending a response of 'Post'
 });
 
-app.post('/task', function (req, res) {
-    const body = req.body;
-    console.log(body);
-    res.send('POST');
-});
-
+// Starting the server and listening on the specified port
 app.listen(port, () => {
-    console.log('APP running on port: ' + port);
+    console.log('APP running on port: ' + port);  // Logging a message indicating the server is running
 });
